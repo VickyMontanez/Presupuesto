c19eb175ccd14a3b2c60688abc3e7398d6ccfb7c
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-04-11 18:38:49 -0500

    refactor: :sparkles: Add a Worker
    The code waschanged to a clear one, reorganizing in a worker

--- storage/wsForm.js
+++ storage/wsForm.js
@@ -1,80 +1,102 @@
 let wsForm ={
+    showNav(){
+        let plantilla = `
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Calcular Presupuesto</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ms-auto">
+                        <a class="nav-link active" href="#">Home</a>
+                        <a class="nav-link active" href="#"> Ingresos</a>
+                        <a class="nav-link active" href="#"> Egresos</a>
+                    </div>
+                </div>
+            </div>`;
+        return plantilla;
+    },
 
-    showInicio(p1){
+    showInicio(){
         let plantilla = `<div class="row d-flex justify-content-center">
                             <div class="col-12 d-flex text-center flex-column mt-3  pt-5 mb-4" style="color: white;">
                                 <h1>Presupuesto Disponible</h1>
                                 <span>-----------------</span>
                             </div>
-                            <div class="col-2 align-items-center">
+                            <div class="col-3 align-items-center">
                                 <div class="d-grid gap-3">
-                                    <div class="p-3 bg-success rounded-3">
+                                    <div class="p-3 bg-success rounded-3 d-flex justify-content-between">
+                                        <p class="fw-bolder text-white">INGRESOS</p>
                                         <h6 id="mostIng"><span class="badge bg-secondary">New</span></h6>
                                     </div>
-                                    <div class="p-3 bg-danger rounded-3">
+                                    <div class="p-3 bg-danger rounded-3 d-flex justify-content-between">
+                                        <p class="fw-bolder text-white">EGRESOS</p>
                                         <h6 id="mostEgr"><span class="badge bg-secondary">New</span></h6>
                                     </div>
                                 </div>
                             </div>
                         </div>`;
-        return plantilla
+        return plantilla;
+    },
+
+    showForm(){
+        let plantilla = `
+            <div class="row flex-nowrap">
+                <div class="col-2">
+                    <select  name="Operación" class="form-select">
+                        <option selected value="none">Operación</option>
+                        <option value="+">+</option>
+                        <option value="-">-</option>
+                    </select>
+                </div>
+                <div class="col-6">
+                    <div class="input-group">
+                        <input name="Tipo" type="text" class="form-control" placeholder="Descripción">
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">$</span>
+                        <input name="Valor" type="number" class="form-control" placeholder="Valor">
+                    </div>
+                </div>
+                <div class="col-1">
+                    <div>
+                        <button class="btn btn-success" type="submit"><i class="bi bi-check-circle"></i></button>
+                    </div>
+                </div>
+            </div>`;
+        return plantilla;
+    },
+
+    showTable(){
+        let plantilla = `
+            <div class="row d-flex justify-content-center">
+                <div class="col-6 text-center">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col" style="color: green ;">Ingresos</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tablaIng">
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-6 text-center">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col" style="color: red;">Egresos</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tablaEgr">
+                        </tbody>
+                    </table>
+                </div>
+            </div>`;
+        return plantilla;
     }
-/*     showForm(){
-        let formulario = document.querySelector("#formulario");
-        let infoForm ={
-            ingresos:[],
-            egresos:[],
-        };
-
-        formulario.addEventListener("submit",(e)=>{
-            e.preventDefault();
-            let data = Object.fromEntries(new FormData(e.target));
-            if(data.Operación == "+"){
-                infoForm["ingresos"].unshift({
-                    operacion:`${data.Operación}`,
-                    tipo: `${data.Tipo}`,
-                    valor: `${data.Valor}`
-                });
-            }else if(data.Operación == "-"){
-                infoForm["egresos"].unshift({
-                    operacion:`${data.Operación}`,
-                    tipo: `${data.Tipo}`,
-                    valor: `${data.Valor}`
-                });
-            }else{
-                alert("Escoge una opción valida")
-            }
-            table();
-            console.log(infoForm);
-            formulario.reset();
-        });
-
-        let table = ()=>{
-            document.querySelector('#tablaIng').innerHTML = "";
-            let contIng = 0;
-            infoForm.ingresos.forEach((val,id)=>{
-                document.querySelector('#tablaIng').insertAdjacentHTML("beforeend",
-                `<tr>
-                    <td>${val.tipo}</td>
-                    <td>${val.valor}</td>
-                </tr>`);
-                contIng = contIng + Number(val.valor);
-                document.querySelector('#mostIng').textContent =`${contIng}`
-    
-            });
-            document.querySelector('#tablaEgr').innerHTML = "";
-            let contEgr = 0;
-            infoForm.egresos.forEach((val,id)=>{
-                document.querySelector('#tablaEgr').insertAdjacentHTML("beforeend",
-                `<tr>
-                    <td>${val.tipo}</td> 
-                    <td>${val.valor}</td>
-                </tr>`);
-                contEgr = contEgr + Number(val.valor);
-                document.querySelector('#mostEgr').textContent = `${contEgr}`
-            });
-        }
-    } */
 }
 
 self.addEventListener("message",(e)=>{
