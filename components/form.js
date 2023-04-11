d3c89b3b7d46ddbb6789258c7d3d7a00aa2e29cc
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-04-11 18:40:20 -0500

    refactor: :sparkles: Add a Worker
    The code was changed to a clear one, reorganizing in a worker

--- components/form.js
+++ components/form.js
@@ -7,14 +7,13 @@ export default{
             egresos:[],
         }; 
         
-        let contIng = 0; 
-        let contEgr = 0;
+        let contIng = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }); 
+        let contEgr = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' });
         let Total;
 
         formulario.addEventListener("submit",(e)=>{
             e.preventDefault();
             let data = Object.fromEntries(new FormData(e.target));
-            data.Valor = Number(data.Valor);
             if(data.Operación == "+"){
                 infoForm["ingresos"].unshift({
                     operacion:`${data.Operación}`,
@@ -28,11 +27,11 @@ export default{
                         <td>${val.tipo}</td>
                         <td>${val.valor}</td>
                     </tr>`); })
-                for (let i of infoForm["ingresos"][0]["valor"]) {
-                        contIng += (i)
-                        console.log(contIng);
-                        document.querySelector('#mostIng').textContent =`${contIng}`
-                }
+
+                contIng = infoForm["ingresos"].reduce(function(cont,value){
+                    return Number(cont) + Number(value.valor)
+                },0);
+                document.querySelector('#mostIng').textContent =`${contIng}`
             }else if(data.Operación == "-"){
                 infoForm["egresos"].unshift({
                     operacion:`${data.Operación}`,
@@ -40,17 +39,17 @@ export default{
                     valor: `${data.Valor}`
                 });
                 document.querySelector('#tablaEgr').innerHTML = "";
-                infoForm.ingresos.forEach((val,id)=>{
+                infoForm.egresos
+                .forEach((val,id)=>{
                 document.querySelector('#tablaEgr').insertAdjacentHTML("beforeend",
                     `<tr>
                         <td>${val.tipo}</td>
                         <td>${val.valor}</td>
                     </tr>`); })
-                for (let i of infoForm["egresos"][0]["valor"]) {
-                        contEgr += (i)
-                        console.log(contEgr);
-                        document.querySelector('#mostEgr').textContent =`${contEgr}`
-                }
+                contEgr = infoForm["egresos"].reduce(function(cont,value){
+                        return Number(cont) + Number(value.valor)
+                },0);
+                document.querySelector('#mostEgr').textContent =`${contEgr}`
             }else{
                 alert("Escoge una opción valida")
             }
@@ -60,26 +59,17 @@ export default{
         });
 
 
-/*         let table =()=>{
-            document.querySelector('#tablaEgr').innerHTML = "";
-            infoForm.egresos.forEach((val,id)=>{
-                document.querySelector('#tablaEgr').insertAdjacentHTML("beforeend",
-                `<tr>
-                    <td>${val.tipo}</td> 
-                    <td>${val.valor}</td>
-                </tr>`);
-                contEgr = contEgr + parseInt(val.valor);
-                document.querySelector('#mostEgr').textContent = `${contEgr}`
-            });
-
-            Total = contIng - contEgr;
-        } */
-
         const ws = new Worker("storage/wsForm.js", {type:"module"});
         let id = [];
         let count = 0;
-        id.push("#inicio")
+        id.push("#inicio");
         ws.postMessage({module:"showInicio"});
+        id.push("#formulario");
+        ws.postMessage({module: "showForm"});
+        id.push("#tabla");
+        ws.postMessage({module: "showTable"})
+        id.push("#nav");
+        ws.postMessage({module:"showNav"})
         ws.addEventListener("message", (e) =>{
 
             let doc = new DOMParser().parseFromString(e.data, "text/html");
@@ -88,8 +78,8 @@ export default{
             dom.append(...doc.body.children);
             (id.length-1 == count) ? ws.terminate() :count++
             
-        })
-        return infoForm
+        });
+        return infoForm;
     }
 
 }
