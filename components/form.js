4a92c13db3c1c7eea5f0fdedd85313cd9138776b
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-04-11 23:10:17 -0500

    feat: :lipstick: Add styles

--- components/form.js
+++ components/form.js
@@ -2,11 +2,6 @@ import config from "../storage/config.js"
 export default{
     showForm(){
         let formulario = document.querySelector("#formulario");
-        let infoForm ={
-            ingresos:[],
-            egresos:[],
-        }; 
-        
         let contIng = 0; 
         let contEgr = 0;
         let Total;
@@ -16,48 +11,58 @@ export default{
             e.preventDefault();
             let data = Object.fromEntries(new FormData(e.target));
             if(data.Operación == "+"){
-                infoForm["ingresos"].unshift({
+                config["ingresos"].unshift({
                     operacion:`${data.Operación}`,
                     tipo: `${data.Tipo}`,
                     valor: `${data.Valor}`
                 });
                 document.querySelector('#tablaIng').innerHTML = "";
-                infoForm.ingresos.forEach((val,id)=>{
+                config.ingresos.forEach((val,id)=>{
                 document.querySelector('#tablaIng').insertAdjacentHTML("beforeend",
-                    `<tr>
-                        <td>${val.tipo}</td>
-                        <td>${val.valor}</td>
+                    `<tr class="ing">
+                        <td class="ing">${val.tipo}</td>
+                        <td class="ing">${val.valor}&nbsp
+                            <i class="eliminar bi bi-x-circle text-danger"></i>
+                        </td>
+
                     </tr>`); })
 
-                contIng = infoForm["ingresos"].reduce(function(cont,value){
+                contIng = config["ingresos"].reduce(function(cont,value){
                     return Number(cont) + Number(value.valor)
                 },0);
                 document.querySelector('#mostIng').textContent =`${contIng}`;
 
+                localStorage.setItem("ingresos", JSON.stringify(config["ingresos"]));
+
             }else if(data.Operación == "-"){
-                infoForm["egresos"].unshift({
+                config["egresos"].unshift({
                     operacion:`${data.Operación}`,
                     tipo: `${data.Tipo}`,
                     valor: `${data.Valor}`
                 });
                 document.querySelector('#tablaEgr').innerHTML = "";
-                contEgr = infoForm["egresos"].reduce(function(cont,value){
+                contEgr = config["egresos"].reduce(function(cont,value){
                     return Number(cont) + Number(value.valor)
                 },0);
                 document.querySelector('#mostEgr').textContent =`${contEgr}`;
 
                 porcentaje = function porcent(n1, n2){
                 return (Number(n1)/ Number(n2)*100).toFixed(3)};
-                var porResultado = porcentaje(infoForm["egresos"][0]["valor"], contEgr);
+                var porResultado = porcentaje(config["egresos"][0]["valor"], contEgr);
 
-                infoForm.egresos.forEach((val,id)=>{
-                document.querySelector('#tablaEgr').insertAdjacentHTML("beforeend",
-                    `<tr>
-                        <td>${val.tipo}</td>
-                        <td>${val.valor}
-                            <span id="mostpor" class="badge bg-secondary">${porResultado}%</span>
-                        </td>
-                    </tr>`); })
+                config.egresos.forEach((val,id)=>{
+                    document.querySelector('#tablaEgr').insertAdjacentHTML("beforeend",
+                        `<tr class="egr">
+                            <td class="egr">${val.tipo}</td>
+                            <td class="egr">${val.valor}&nbsp
+                                <span id="mostpor" class="badge bg-secondary">${porResultado}%</span>
+                                <i class="eliminar bi bi-x-circle text-danger"></i>
+                            </td>
+                            
+                        </tr>`); 
+                });
+                
+                localStorage.setItem("egresos", JSON.stringify(config["egresos"]));
 
             }else{
                 alert("Escoge una opción valida")
